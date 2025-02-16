a7b5dcf4e61b1123d96ebbe50f67c37c12dc1e98
cemakpolat <cem.akpolat@sva.de>
2025-02-16 12:02:07 +0100

    esm change

--- src/main/main.js
+++ src/main/main.js
@@ -1,14 +1,22 @@
-const { app, BrowserWindow, ipcMain } = require('electron');
-const path = require('path');
+import { app, BrowserWindow, ipcMain } from 'electron';
+import path from 'path';
+import electronStore from 'electron-store';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+// Define __dirname for ESM
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 let mainWindow;
 
 function createWindow() {
   mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
-    frame: false, // Disable the default window frame
+    frame: false,
     webPreferences: {
-      preload: path.join(__dirname, '../preload.js'), // Optional: Use preload.js for secure communication
+      preload: path.join(__dirname, '../preload.js'),
       nodeIntegration: true,
       contextIsolation: false,
     },
@@ -16,15 +24,10 @@ function createWindow() {
 
   mainWindow.loadFile('src/renderer/index.html');
 
-   // Open DevTools (optional) // Debugging
-// mainWindow.webContents.openDevTools();
-
-// Handle close event
   ipcMain.on('close-window', () => {
     mainWindow.close();
   });
 
-  // Handle minimize event
   ipcMain.on('minimize-window', () => {
     mainWindow.minimize();
   });
